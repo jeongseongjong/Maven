a6516f56e5823f2344a21e4cb4f0fb1b660ebe62
jeongseongjong <jjong9950@naver.com>
2019-11-14 13:18:54 +0900

    2019-11-14

---
+++ Maven_60_build/src/main/java/com/biz/build/MyExec_01.java
@@ -0,0 +1,10 @@
+package com.biz.build;
+
+public class MyExec_01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("MyExec");
+		System.out.println("Republic of Korea");
+	}
+}
